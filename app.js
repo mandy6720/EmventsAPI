ef5655877a201ae1c7356f9574e9cefc1c9e701c
Ngo Thieu Kieu Chinh <ngothieukieuchinh@MacBook-Pro.local>
2017-03-22 07:09:01 +0700

    create array of events

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+let events = [
+  {id:1, title:"Tittle 1", description:"description 1", date:"04012017"},
+  {id:2, title:"Tittle 2", description:"description 2", date:"04012017"},
+  {id:3, title:"Tittle 3", description:"description 3", date:"04012017"}
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
