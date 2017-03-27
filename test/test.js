3bc03897921cd701b16b01d5e60bb9f7abf1b80b
Amanda Hewitt <amanda.hewitt01@gmail.com>
2017-03-26 20:45:48 -0400

    Tests the GET /events/:id route

--- test/test.js
+++ test/test.js
@@ -1,21 +1,37 @@
-let chai = require('chai')
-let chaiHttp = require('chai-http')
-let server = require('../app.js')
-let should = chai.should()
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app.js');
+let should = chai.should();
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 //Test the /GET Routes
 describe("This is CRUD of Events", () => {
   describe("GET / all events", () => {
 
-    it("returns status code 200", (done) => {
+    it("returns all events", (done) => {
       chai.request(server)
         .get('/events')
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.a('array')
           res.body.length.should.be.eql(3)
+          done()
+        })
+    });
+
+  });
+
+  // Test /GET by id
+  describe("GET / event by id", () => {
+
+    it("returns one event", (done) => {
+      chai.request(server)
+        .get('/events/1')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          res.body.length.should.be.eql(1)
           done()
         })
     });
