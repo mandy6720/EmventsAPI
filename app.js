f3d43134f828f2a1ec535d1ad525abfb580c22ea
Ngo Thieu Kieu Chinh <ngothieukieuchinh@MacBook-Pro.local>
2017-04-05 07:49:33 +0700

    refactor to use mongoose

--- app.js
+++ app.js
@@ -1,177 +1,151 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongo = require('mongodb')
-const MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 // Connection URL
 const url = 'mongodb://localhost:27017/emvents';
 
-const insertEvent = (data, db, callback, errCb) => {
-  // Get the documents collection
-  let collection = db.collection('events');
-  // Insert some documents
-  collection.insertOne(data, (err, result) => {
-    if (err) {
-      errCb(err);
-    } else {
-      console.log("Successfully inserted event!");
-      callback(result);
-    }
-  });
-};
+// Connectect to DB
+mongoose.connect(url)
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
 
-const getAllEvents = (db, callback, errCb) => {
-  // Get the documents collection
-  let collection = db.collection('events');
-  // Insert some documents
-  collection.find({}).toArray((err, events) => {
-    if (err) {
-      errCb(err);
-    } else {
-      console.log("Found events!");
-      callback(events);
-    }
-  });
-};
+// Create Event schema
+let EventSchema = new Schema(
+  {
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    date: {type: Date, required: true, default: Date.now }
+  }
+)
 
-const getEvent = (eventId, db, callback, errCb) => {
-  // Get the documents collection
-  let collection = db.collection('events');
-  // Insert some documents
-  let objectId = new mongo.ObjectId(eventId);
-  collection.findOne({'_id': objectId}, (err, event) =>  {
-    if (err) {
-      errCb(err);
-    } else {
-      console.log("Found event!");
-      callback(event);
-    }
-  });
-};
+// Create Event model
+let Event = mongoose.model('Event', EventSchema)
 
-const updateEvent = function(eventId, data, db, callback, errCb) {
-  // Get the events collection
-  let collection = db.collection('events');
-  let objectId = new mongo.ObjectId(eventId);
-  // Update document where a is 2, set b equal to 1
-  collection.updateOne({ '_id' : objectId }
-    , { $set: { title : data.title, description: data.description,
-    date: data.date} }, (err, result) => {
-    if (err) {
-      errCb(err);
-    } else {
-      callback(result);
-    }
-  });
-}
 
-const removeEvent = (eventId, db, callback, errCb) => {
-  // Get the documents collection
-  let collection = db.collection('events');
-  let objectId = new mongo.ObjectId(eventId);
-  // Delete document where a is 3
-  collection.deleteOne({ _id : objectId }, (err, result) => {
-    if (err) {
-      errCb(err);
-    } else {
-      callback(result);
-    }
-  });
-}
+// const insertEvent = (data, db, callback, errCb) => {
+//   // Get the documents collection
+//   let collection = db.collection('events');
+//   // Insert some documents
+//   collection.insertOne(data, (err, result) => {
+//     if (err) {
+//       errCb(err);
+//     } else {
+//       console.log("Successfully inserted event!");
+//       callback(result);
+//     }
+//   });
+// };
+//
+// const getAllEvents = (db, callback, errCb) => {
+//   // Get the documents collection
+//   let collection = db.collection('events');
+//   if (err) {
+//     errCb(err);
+//   } else {
+//     console.log("Found events!");
+//     callback(events);
+//   }
+//   // Insert some documents
+//   collection.find({}).toArray((err, events) => {
+//
+//   });
+// };
+//
+// const getEvent = (eventId, db, callback, errCb) => {
+//   // Get the documents collection
+//   let collection = db.collection('events');
+//   // Insert some documents
+//   let objectId = new mongo.ObjectId(eventId);
+//   collection.findOne({'_id': objectId}, (err, event) =>  {
+//     if (err) {
+//       errCb(err);
+//     } else {
+//       console.log("Found event!");
+//       callback(event);
+//     }
+//   });
+// };
+//
+// const updateEvent = function(eventId, data, db, callback, errCb) {
+//   // Get the events collection
+//   let collection = db.collection('events');
+//   let objectId = new mongo.ObjectId(eventId);
+//   // Update document where a is 2, set b equal to 1
+//   collection.updateOne({ '_id' : objectId }
+//     , { $set: { title : data.title, description: data.description,
+//     date: data.date} }, (err, result) => {
+//     if (err) {
+//       errCb(err);
+//     } else {
+//       callback(result);
+//     }
+//   });
+// }
+//
+// const removeEvent = (eventId, db, callback, errCb) => {
+//   // Get the documents collection
+//   let collection = db.collection('events');
+//   let objectId = new mongo.ObjectId(eventId);
+//   // Delete document where a is 3
+//   collection.deleteOne({ _id : objectId }, (err, result) => {
+//     if (err) {
+//       errCb(err);
+//     } else {
+//       callback(result);
+//     }
+//   });
+// }
 
 // Middleware
 app.use(bodyParser.json()); // for parsing application/json
 
 // Routes
 app.get('/events', (req, res) => {
-  // connect to Mongo
-  // Use connect method to connect to the server
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to Mongo");
-    getAllEvents(db, (event) => {
-      res.status(200).send(event);
-      db.close();
-    }, (err) => {
-      res.status(404).send(err);
-      db.close();
-    });
-  });
+  Event.find({}, (err, events) => {
+
+    if (err) res.status(404).send(err)
+    res.status(200).send(events)
+  })
 });
 
 app.get('/events/:id', (req, res) => {
 	let eventId = req.params.id;
-
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to Mongo");
-    getEvent(eventId, db, (event) => {
-      res.status(200).send(event);
-      db.close();
-    }, (err) => {
-      res.status(404).send(err);
-      db.close();
-    });
-  });
+  Event.findById({_id: eventId}, (err, event) => {
+    if (err) res.status(404).send(err)
+    res.status(200).send(event)
+  })
 });
 
 app.post('/events', (req, res) => {
-  let newEvent = {
-    title: req.body.title,
-    description: req.body.description,
-    date: req.body.date
-  };
-  // Connect to Mongo
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to Mongo");
-    insertEvent(newEvent, db, function(result) {
-      res.status(200).send({message: "Event successfully added!"});
-      db.close();
-    }, (err) => {
-      res.status(400).send(err);
-    });
-  });
+  let newEvent = new Event(req.body)
+  newEvent.save((err,event) => {
+    if (err) res.status(400).send(err)
+    res.status(200).send({ message: 'Created!', event })
+  })
 });
 
 app.put('/events/:id', (req, res) => {
   const eventId = req.params.id
-  let newEvent = {
-    title: req.body.title,
-    description: req.body.description,
-    date: req.body.date
-  };
 
-  // Connect to Mongo
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to Mongo");
-    updateEvent(eventId, newEvent, db, (result) => {
-      res.status(200).send({message: "Event successfully updated!"});
-      db.close();
-    }, (err) => {
-      res.status(400).send(err);
+  Event.findById({_id: eventId}, (err, event) => {
+        if(err) res.send(err);
+        Object.assign(event, req.body).save((err, event) => {
+            if(err) res.send(err);
+            res.json({ message: 'Updated!', event });
+        });
     });
-  });
-
 })
 
 app.delete("/events/:id", (req,res) => {
   const eventId = req.params.id
 
-  // Connect to Mongo
-  MongoClient.connect(url, (err, db) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to Mongo");
-    removeEvent(eventId, db, (result) => {
-      res.status(200).send({message: "Event successfully deleted!"});
-      db.close();
-    }, (err) => {
-      res.status(400).send(err);
-    });
-  });
+  Event.remove({_id: eventId}, (err) => {
+    if (err) res.send(err)
+    res.json({message: 'Deleted'})
+  })
 })
 
 app.listen(3000, () => {
